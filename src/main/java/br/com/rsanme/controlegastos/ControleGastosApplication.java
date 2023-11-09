6f3b7354cea64ea4065d7139d0425a6199647562
regissanme <rsanme@gmail.com>
2023-11-09 14:25:19 -0300

    initing project

---
+++ src/main/java/br/com/rsanme/controlegastos/ControleGastosApplication.java
@@ -0,0 +1,13 @@
+package br.com.rsanme.controlegastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleGastosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rsanme/controlegastos/ControleGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rsanme.controlegastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
