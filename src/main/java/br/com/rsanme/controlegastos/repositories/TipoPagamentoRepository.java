601ba0eec3f086b12d41c0cee054ba6a56cffb4e
regissanme <rsanme@gmail.com>
2023-11-09 16:44:26 -0300

    creating payment type repository

---
+++ src/main/java/br/com/rsanme/controlegastos/repositories/TipoPagamentoRepository.java
@@ -0,0 +1,15 @@
+package br.com.rsanme.controlegastos.repositories;
+
+import br.com.rsanme.controlegastos.models.TipoPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 09/11/2023
+ * Hora: 16:43
+ */
+@Repository
+public interface TipoPagamentoRepository extends JpaRepository<TipoPagamento, Long> {
+}
