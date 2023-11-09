bc86ad201e79acbbfd35541c0929c79e9d574c59
regissanme <rsanme@gmail.com>
2023-11-09 18:04:48 -0300

    creating payment type service implementation find all test

--- src/test/java/br/com/rsanme/controlegastos/services/impl/TipoPagamentoServiceImplTest.java
+++ src/test/java/br/com/rsanme/controlegastos/services/impl/TipoPagamentoServiceImplTest.java
@@ -1,10 +1,18 @@
 package br.com.rsanme.controlegastos.services.impl;
 
+import br.com.rsanme.controlegastos.models.TipoPagamento;
+import br.com.rsanme.controlegastos.repositories.TipoPagamentoRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Projeto: controle-gastos
@@ -18,6 +26,12 @@ class TipoPagamentoServiceImplTest {
     @InjectMocks
     private TipoPagamentoServiceImpl service;
 
+    private TipoPagamento tipoPagamento;
+    private Optional<TipoPagamento> optionalTipoPagamento;
+
+    @Mock
+    private TipoPagamentoRepository repository;
+
     @BeforeEach
     void setUp() {
         createInstances();
@@ -26,7 +40,17 @@ class TipoPagamentoServiceImplTest {
 
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnList() {
+        List<TipoPagamento> list = List.of(tipoPagamento);
+
+        Mockito.when(repository.findAll()).thenReturn(list);
+
+        List<TipoPagamento> response = service.findAll();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(1, response.size());
+        Assertions.assertEquals(TipoPagamento.class, response.get(0).getClass());
+        Assertions.assertEquals(tipoPagamento, response.get(0));
     }
 
     @Test
@@ -46,5 +70,7 @@ class TipoPagamentoServiceImplTest {
     }
 
     private void createInstances() {
+        tipoPagamento = new TipoPagamento(1L, "Dinheiro");
+        optionalTipoPagamento = Optional.of(tipoPagamento);
     }
 }
