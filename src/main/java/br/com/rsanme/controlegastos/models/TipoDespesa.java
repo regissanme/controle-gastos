691d2a1cfd7e94eef128d2f8a03cd46bc08d3a58
regissanme <rsanme@gmail.com>
2023-11-09 16:37:51 -0300

    cleaning classes

--- src/main/java/br/com/rsanme/controlegastos/models/TipoDespesa.java
+++ src/main/java/br/com/rsanme/controlegastos/models/TipoDespesa.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -25,4 +27,25 @@ public class TipoDespesa {
     @JoinColumn(name = "categoria_despesa_id")
     private CategoriaDespesa categoriaDespesa;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TipoDespesa that = (TipoDespesa) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "TipoDespesa{" +
+                "id=" + id +
+                ", descricao='" + descricao + '\'' +
+                ", categoriaDespesa=" + categoriaDespesa +
+                '}';
+    }
 }
--- src/main/java/br/com/rsanme/controlegastos/models/TipoPagamento.java
+++ src/main/java/br/com/rsanme/controlegastos/models/TipoPagamento.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @Entity
@@ -20,4 +22,25 @@ public class TipoPagamento {
 
     @Column(unique = true, nullable = false)
     private String tipo;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TipoPagamento that = (TipoPagamento) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "TipoPagamento{" +
+                "id=" + id +
+                ", tipo='" + tipo + '\'' +
+                '}';
+    }
 }
