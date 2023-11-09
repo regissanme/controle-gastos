21226cf04dfaf43c301d6367b49b0ee41b8231e6
regissanme <rsanme@gmail.com>
2023-11-09 17:20:24 -0300

    creating payment type service implementation tests

---
+++ src/test/java/br/com/rsanme/controlegastos/services/impl/TipoPagamentoServiceImplTest.java
@@ -0,0 +1,50 @@
+package br.com.rsanme.controlegastos.services.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 09/11/2023
+ * Hora: 17:11
+ */
+@ExtendWith(MockitoExtension.class)
+class TipoPagamentoServiceImplTest {
+
+    @InjectMocks
+    private TipoPagamentoServiceImpl service;
+
+    @BeforeEach
+    void setUp() {
+        createInstances();
+    }
+
+
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void createInstances() {
+    }
+}
