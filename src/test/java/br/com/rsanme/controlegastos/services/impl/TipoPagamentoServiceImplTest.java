d0de8568b8d0569cf0f226ec3af3512fb59a2954
regissanme <rsanme@gmail.com>
2023-11-09 20:54:00 -0300

    creating payment type service implementation update tests

--- src/test/java/br/com/rsanme/controlegastos/services/impl/TipoPagamentoServiceImplTest.java
+++ src/test/java/br/com/rsanme/controlegastos/services/impl/TipoPagamentoServiceImplTest.java
@@ -135,10 +135,91 @@ class TipoPagamentoServiceImplTest {
 
     @Test
     void whenUpdateThenReturnSuccess() {
+
+        when(repository.findById(anyLong())).thenReturn(optionalTipoPagamento);
+        when(repository.findByTipo(anyString())).thenReturn(optionalTipoPagamento);
+        when(repository.save(any())).thenReturn(tipoPagamento);
+
+        TipoPagamento response = service.update(tipoPagamento);
+
+        assertNotNull(response);
+        assertEquals(TipoPagamento.class, response.getClass());
+        assertEquals(tipoPagamento.getId(), response.getId());
+        assertEquals(tipoPagamento.getTipo(), response.getTipo());
+
+        verify(repository, times(1))
+                .save(any());
+    }
+
+    @Test
+    void whenUpdateThenThrowsNotFound() {
+
+        String errorMessage = String.format("Tipo de Pagamento com Id %s não encontrado!", ID);
+
+        assertThatThrownBy(() -> service.update(tipoPagamento))
+                .hasMessage(errorMessage)
+                .isInstanceOf(EntityNotFoundException.class);
+
+        verify(repository, times(1))
+                .findById(anyLong());
+
+        verify(repository, never())
+                .save(any());
+    }
+
+    @Test
+    void whenUpdateThenThrowsAlreadyExists() {
+
+        TipoPagamento paraAtualizar = new TipoPagamento();
+        paraAtualizar.setId(2L);
+        paraAtualizar.setTipo(TIPO_PAGAMENTO_DINHEIRO);
+
+        String errorMessage = String.format(
+                "Um Tipo de Pagamento com o tipo %s já existe!", TIPO_PAGAMENTO_DINHEIRO);
+
+        when(repository.findById(anyLong())).thenReturn(optionalTipoPagamento);
+        when(repository.findByTipo(anyString())).thenReturn(optionalTipoPagamento);
+
+        assertThatThrownBy(() -> service.update(paraAtualizar))
+                .hasMessage(errorMessage)
+                .isInstanceOf(EntityExistsException.class);
+
+        verify(repository, times(1))
+                .findByTipo(TIPO_PAGAMENTO_DINHEIRO);
+
+        verify(repository, never())
+                .save(any());
     }
 
     @Test
     void whenDeleteThenSuccess() {
+
+        when(repository.findById(anyLong())).thenReturn(optionalTipoPagamento);
+
+        service.delete(ID);
+
+        verify(repository, times(1))
+                .findById(anyLong());
+
+        verify(repository, times(1))
+                .deleteById(anyLong());
+
+    }
+
+    @Test
+    void whenDeleteThenThrowsNotFound() {
+
+        String errorMessage = String.format("Tipo de Pagamento com Id %s não encontrado!", ID);
+
+        assertThatThrownBy(() -> service.delete(ID))
+                .hasMessage(errorMessage)
+                .isInstanceOf(EntityNotFoundException.class);
+
+        verify(repository, times(1))
+                .findById(anyLong());
+
+        verify(repository, never())
+                .deleteById(anyLong());
     }
 
     private void createInstances() {
