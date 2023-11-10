503e540933486e2f236d25b4ba29965189b5625e
regissanme <rsanme@gmail.com>
2023-11-10 10:49:57 -0300

    create save on expense type service implementation

--- src/main/java/br/com/rsanme/controlegastos/services/impl/TipoDespesaServiceImpl.java
+++ src/main/java/br/com/rsanme/controlegastos/services/impl/TipoDespesaServiceImpl.java
@@ -5,6 +5,7 @@ import br.com.rsanme.controlegastos.repositories.TipoDespesaRepository;
 import br.com.rsanme.controlegastos.services.ITipoDespesaService;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintDefinitionException;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +16,7 @@ import java.util.Optional;
  * Data: 10/11/2023
  * Hora: 10:07
  */
-public class TipoDespesaServiceImpl implements ITipoDespesaService {
+public class TipoDespesaServiceImpl implements ITipoDespesaService<TipoDespesa> {
 
     private final TipoDespesaRepository repository;
 
@@ -23,13 +24,14 @@ public class TipoDespesaServiceImpl implements ITipoDespesaService {
         this.repository = repository;
     }
 
+
     @Override
-    public List findAll() {
+    public List<TipoDespesa> findAll() {
         return repository.findAll();
     }
 
     @Override
-    public Object findById(Long id) {
+    public TipoDespesa findById(Long id) {
         return repository.findById(id)
                 .orElseThrow(
                         () -> new EntityNotFoundException(
@@ -38,25 +40,37 @@ public class TipoDespesaServiceImpl implements ITipoDespesaService {
     }
 
     @Override
-    public Object create(Object o) {
-        return null;
+    public TipoDespesa create(TipoDespesa tipoDespesa) {
+        if (tipoDespesa.getCategoriaDespesa() == null) {
+            throw new ConstraintDefinitionException(
+                    "Dados inválidos!"
+            );
+        }
+
+        findByDescricao(tipoDespesa);
+
+        return repository.save(tipoDespesa);
     }
 
     @Override
-    public Object update(Object o) {
-        return null;
+    public TipoDespesa update(TipoDespesa tipoDespesa) {
+        findById(tipoDespesa.getId());
+        findByDescricao(tipoDespesa);
+
+        return repository.save(tipoDespesa);
     }
 
     @Override
     public void delete(Long id) {
-
+        findById(id);
+        repository.deleteById(id);
     }
 
-    private void findByDescricao(TipoDespesa tipoDespesa){
+    private void findByDescricao(TipoDespesa tipoDespesa) {
 
         Optional<TipoDespesa> byDescricao = repository.findByDescricao(tipoDespesa.getDescricao());
 
-        if(byDescricao.isPresent() && !byDescricao.get().getId().equals(tipoDespesa.getId())){
+        if (byDescricao.isPresent() && !byDescricao.get().getId().equals(tipoDespesa.getId())) {
             throw new EntityExistsException(
                     String.format(
                             "Um Tipo de Despesa com a descrição %s já existe!", tipoDespesa.getDescricao()
