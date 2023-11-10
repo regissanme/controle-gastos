da3d2a5236432b397a6e4006c062a6b33b04d101
regissanme <rsanme@gmail.com>
2023-11-10 13:52:40 -0300

    create test for main context

--- src/test/java/br/com/rsanme/controlegastos/ControleGastosApplicationTests.java
+++ src/test/java/br/com/rsanme/controlegastos/ControleGastosApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ControleGastosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        ControleGastosApplication.main(new String[]{});
+    }
 
 }
