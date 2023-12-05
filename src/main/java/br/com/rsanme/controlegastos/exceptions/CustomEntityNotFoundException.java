2f072db7c09a5c1bcd7eee30fd642dd0591cf398
regissanme <rsanme@gmail.com>
2023-12-04 23:13:19 -0300

    feature: create custom entity not found exception extending business exception

---
+++ src/main/java/br/com/rsanme/controlegastos/exceptions/CustomEntityNotFoundException.java
@@ -0,0 +1,13 @@
+package br.com.rsanme.controlegastos.exceptions;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 04/12/2023
+ * Hora: 23:12
+ */
+public class CustomEntityNotFoundException extends BusinessExceptions {
+    public CustomEntityNotFoundException(String message) {
+        super(message);
+    }
+}
