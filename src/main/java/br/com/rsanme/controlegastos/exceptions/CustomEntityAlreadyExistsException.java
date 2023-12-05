8f18c8be87faca2f2a623dfcaa600886ee392304
regissanme <rsanme@gmail.com>
2023-12-04 23:14:26 -0300

    feature: create custom entity already exists exception extending business exception

---
+++ src/main/java/br/com/rsanme/controlegastos/exceptions/CustomEntityAlreadyExistsException.java
@@ -0,0 +1,13 @@
+package br.com.rsanme.controlegastos.exceptions;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 04/12/2023
+ * Hora: 23:13
+ */
+public class CustomEntityAlreadyExistsException extends BusinessExceptions {
+    public CustomEntityAlreadyExistsException(String message) {
+        super(message);
+    }
+}
