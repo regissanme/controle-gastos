eb33cf39eaaa1da2ff63963a6e28c21389723b40
regissanme <rsanme@gmail.com>
2023-12-04 23:15:48 -0300

    feature: create custom username not found exception extending business exception

---
+++ src/main/java/br/com/rsanme/controlegastos/exceptions/CustomUsernameNotFoundException.java
@@ -0,0 +1,13 @@
+package br.com.rsanme.controlegastos.exceptions;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 04/12/2023
+ * Hora: 23:15
+ */
+public class CustomUsernameNotFoundException extends BusinessExceptions {
+    public CustomUsernameNotFoundException(String message) {
+        super(message);
+    }
+}
