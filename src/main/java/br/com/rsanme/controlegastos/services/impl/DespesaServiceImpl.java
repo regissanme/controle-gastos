e93a1d2f2415b07429c15eccc61490277da566ba
regissanme <rsanme@gmail.com>
2023-12-04 23:06:16 -0300

    feature: create expense service implementation

---
+++ src/main/java/br/com/rsanme/controlegastos/services/impl/DespesaServiceImpl.java
@@ -0,0 +1,52 @@
+package br.com.rsanme.controlegastos.services.impl;
+
+import br.com.rsanme.controlegastos.models.Despesa;
+import br.com.rsanme.controlegastos.repositories.DespesaRepository;
+import br.com.rsanme.controlegastos.services.IDespesaService;
+import jakarta.persistence.EntityNotFoundException;
+
+import java.util.List;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 04/12/2023
+ * Hora: 22:43
+ */
+public class DespesaServiceImpl implements IDespesaService<Despesa> {
+
+    private final DespesaRepository repository;
+
+    public DespesaServiceImpl(DespesaRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Despesa> findAll(Long id) {
+        return repository.findAll();
+    }
+
+    @Override
+    public Despesa findById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Despesa não encontrada com Id: " + id)
+                );
+    }
+
+    @Override
+    public Despesa create(Despesa despesa) {
+        return repository.save(despesa);
+    }
+
+    @Override
+    public Despesa update(Despesa despesa) {
+        return repository.save(despesa);
+    }
+
+    @Override
+    public void delete(Long id) {
+        findById(id);
+        repository.deleteById(id);
+    }
+}
