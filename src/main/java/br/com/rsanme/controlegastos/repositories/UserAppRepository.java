0eaf43f6fcbab5ec7c5294a7d2667d1e723be6c1
regissanme <rsanme@gmail.com>
2023-12-16 17:38:29 -0300

    feature: create user app repository

---
+++ src/main/java/br/com/rsanme/controlegastos/repositories/UserAppRepository.java
@@ -0,0 +1,14 @@
+package br.com.rsanme.controlegastos.repositories;
+
+import br.com.rsanme.controlegastos.models.UserApp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 16/12/2023
+ * Hora: 17:35
+ */
+public interface UserAppRepository extends JpaRepository<UserApp, Long> {
+
+}
