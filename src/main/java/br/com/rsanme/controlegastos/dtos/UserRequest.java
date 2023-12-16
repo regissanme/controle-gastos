58437883ba6d4cf536cd2ffe38bc2e5c8c0f227a
regissanme <rsanme@gmail.com>
2023-12-16 17:32:20 -0300

    feature: create user app request and response dto classes

---
+++ src/main/java/br/com/rsanme/controlegastos/dtos/UserRequest.java
@@ -0,0 +1,46 @@
+package br.com.rsanme.controlegastos.dtos;
+
+import br.com.rsanme.controlegastos.models.UserApp;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.Date;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 16/12/2023
+ * Hora: 16:41
+ */
+@Data
+public class UserRequest {
+
+    private Long id;
+
+    @NotBlank(message = "O usuário é obrigatório!")
+    private String username;
+
+    @NotBlank(message = "A senha é obrigatória!")
+    private String password;
+
+    @NotBlank(message = "O nome é obrigatório!")
+    private String name;
+
+    @NotBlank(message = "O cpf é obrigatório!")
+    private String cpf;
+
+    private Date birthDate;
+
+    public UserApp toModel(){
+        UserApp userApp = new UserApp();
+        userApp.setId(id);
+        userApp.setUsername(username);
+        userApp.setPassword(password);
+        userApp.setName(name);
+        userApp.setCpf(cpf);
+        userApp.setBirthDate(birthDate);
+
+        return userApp;
+    }
+}
---
+++ src/main/java/br/com/rsanme/controlegastos/dtos/UserResponse.java
@@ -0,0 +1,47 @@
+package br.com.rsanme.controlegastos.dtos;
+
+import br.com.rsanme.controlegastos.models.UserApp;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 16/12/2023
+ * Hora: 16:41
+ */
+@Data
+public class UserResponse {
+
+    private Long id;
+    private String username;
+    private String name;
+    private String cpf;
+    private String role;
+    private Boolean active;
+    private Date birthDate;
+    private LocalDateTime lastAccessAt;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String token;
+
+    public static UserResponse toResponse(UserApp userApp, String token) {
+
+        UserResponse response = new UserResponse();
+        response.setId(userApp.getId());
+        response.setUsername(userApp.getUsername());
+        response.setName(userApp.getName());
+        response.setCpf(userApp.getCpf());
+        response.setRole(userApp.getRole());
+        response.setActive(userApp.getActive());
+        response.setBirthDate(userApp.getBirthDate());
+        response.setLastAccessAt(userApp.getLastAccessAt());
+        response.setToken(token);
+
+        return response;
+    }
+}
