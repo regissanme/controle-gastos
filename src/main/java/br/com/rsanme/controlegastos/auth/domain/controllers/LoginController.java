acda9c210948685958a3e476c747af5e8f4a67d5
regissanme <rsanme@gmail.com>
2023-12-19 00:45:51 -0300

    feature: create login controller

---
+++ src/main/java/br/com/rsanme/controlegastos/auth/domain/controllers/LoginController.java
@@ -0,0 +1,51 @@
+package br.com.rsanme.controlegastos.auth.domain.controllers;
+
+import br.com.rsanme.controlegastos.auth.domain.dtos.UserResponse;
+import br.com.rsanme.controlegastos.auth.domain.models.Login;
+import br.com.rsanme.controlegastos.auth.domain.models.UserApp;
+import br.com.rsanme.controlegastos.auth.domain.services.impl.UserAppService;
+import br.com.rsanme.controlegastos.auth.jwt.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 18/12/2023
+ * Hora: 23:59
+ */
+@RestController
+@RequestMapping("api/v1/login")
+public class LoginController {
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+    private final UserAppService userAppService;
+
+    public LoginController(AuthenticationManager authenticationManager, TokenService tokenService, UserAppService userAppService) {
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+        this.userAppService = userAppService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> login(@RequestBody @Valid Login login) {
+
+        UsernamePasswordAuthenticationToken authenticationToken
+                = new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword());
+        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
+        var principal = (UserApp) authenticate.getPrincipal();
+        userAppService.setLastAccessAt(principal);
+        String token = tokenService.getToken(principal);
+        UserResponse response = UserResponse.toResponse(principal, token);
+
+        return ResponseEntity.ok(response);
+    }
+}
