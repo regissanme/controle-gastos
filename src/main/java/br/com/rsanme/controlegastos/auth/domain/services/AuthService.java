c42fbc308bba2147de67fdfa450707a85c0a43a7
regissanme <rsanme@gmail.com>
2023-12-18 23:54:46 -0300

    feature: create auth user details service

---
+++ src/main/java/br/com/rsanme/controlegastos/auth/domain/services/AuthService.java
@@ -0,0 +1,32 @@
+package br.com.rsanme.controlegastos.auth.domain.services;
+
+import br.com.rsanme.controlegastos.auth.domain.repositories.UserAppRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 18/12/2023
+ * Hora: 23:47
+ */
+@Service
+public class AuthService implements UserDetailsService {
+
+    private final UserAppRepository repository;
+
+    public AuthService(UserAppRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails user = repository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Não encontrado nenhum usuário com o username: " + username);
+        }
+        return user;
+    }
+}
