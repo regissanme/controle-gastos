31ddcd616daf58997021ca53117ce4854f3da1a5
regissanme <rsanme@gmail.com>
2023-12-19 00:01:47 -0300

    feature: create login class

---
+++ src/main/java/br/com/rsanme/controlegastos/auth/domain/models/Login.java
@@ -0,0 +1,20 @@
+package br.com.rsanme.controlegastos.auth.domain.models;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 16/12/2023
+ * Hora: 17:06
+ */
+@Data
+public class Login {
+
+    @NotBlank(message = "O usuário é obrigatório!")
+    private String username;
+
+    @NotBlank(message = "A senha é obrigatória!")
+    private String password;
+}
