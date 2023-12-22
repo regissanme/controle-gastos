4936ceaaca05ff976ba4bacf729dc9a9d03043e6
regissanme <rsanme@gmail.com>
2023-12-21 22:25:51 -0300

    test: create user app controller tests setup

---
+++ src/test/java/br/com/rsanme/controlegastos/auth/domain/controllers/UserAppControllerTest.java
@@ -0,0 +1,20 @@
+package br.com.rsanme.controlegastos.auth.domain.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * Projeto: controle-gastos
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 21/12/2023
+ * Hora: 22:18
+ */
+class UserAppControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    private void createInstances() {
+
+    }
+}
