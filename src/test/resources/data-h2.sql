dcdfbb5bd7a6fa2f9db29f80a6ac7384f1ef4025
regissanme <rsanme@gmail.com>
2023-12-26 13:35:34 -0300

    test: create user in database to tests

---
+++ src/test/resources/data-h2.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user_app` (`id`,`active`,`birth_date`,`cpf`,`created_at`,`current_access_at`,`last_access_at`,`name`,`password`,`role`,`updated_at`,`username`)
+VALUES ('1','1','1978-11-14','86039598115','2023-12-26 11:08:18.856183',null,null,'Regis Sanme','$2a$10$DHYGBCJFqVGk0F3fDqV2huR2.jmApLBBQchPKXCIQemzvwHXIkMgO','ROLE_ADMIN',null,'regis');
